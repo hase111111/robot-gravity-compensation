de73356f9736775a5a4f67e97f41928835756402
hase111111 <hasehasehase61@gmail.com>
2024-11-22 01:32:35 +0900

    行列の変換を行うモジュールの追加

---
+++ gravibot/_math/covert.py
@@ -0,0 +1,7 @@
+"""provide functions for converting one matrix to another"""
+
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2024 Taisei Hasegawa
+# Released under the MIT license
+# https://opensource.org/licenses/mit-license.php
