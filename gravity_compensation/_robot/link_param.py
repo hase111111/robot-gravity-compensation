692b2f6bd922c1ed08a1a0f4ac3196e8fa51f1b2
hase111111 <hasehasehase61@gmail.com>
2024-11-22 00:09:01 +0900

    プロパティのコンパイルが通らない不具合の修正

--- gravity_compensation/_robot/link_param.py
+++ gravity_compensation/_robot/link_param.py
@@ -6,21 +6,49 @@
 
 
 import numpy as np
-from dataclasses import dataclass
 
 from .._math.trans import get_rot4x4, get_trans4x4, zero_small_values
 from .._math.type import TransMatrix
 
 
-@dataclass(frozen=True)
+def define_property(self, name, *, init_value=None, can_get=True, can_set=True):
+    # "_User__name" のような name mangling 後の名前.
+    field_name = "_{}__{}".format(self.__class__.__name__, name)
+
+    # 初期値を設定する.
+    setattr(self, field_name, init_value)
+
+    # getter/setter を生成し, プロパティを定義する.
+    getter = (lambda self: getattr(self, field_name)) if can_get else None
+    setter = (lambda self, value: setattr(self, field_name, value)) if can_set else None
+    setattr(self.__class__, name, property(getter, setter))
+
+
 class LinkParam:
-    a: float
-    alpha: float
-    d: float
-    theta: float
-    is_rot_axis: bool = True
-    min_val: float = -np.pi
-    max_val: float = np.pi
+    def __init__(
+        self,
+        a: float,
+        alpha: float,
+        d: float,
+        theta: float,
+        *,
+        is_rot_axis: bool = True,
+        min_val: float = -np.pi,
+        max_val: float = np.pi
+    ):
+        define_property(self, "a", init_value=a, can_set=False)
+        define_property(self, "alpha", init_value=alpha, can_set=False)
+
+        define_property(self, "is_rot_axis", init_value=is_rot_axis, can_set=False)
+        define_property(self, "min_val", init_value=min_val, can_set=False)
+        define_property(self, "max_val", init_value=max_val, can_set=False)
+
+        if is_rot_axis:
+            define_property(self, "d", init_value=d, can_set=False)
+            define_property(self, "theta", init_value=theta)
+        else:
+            define_property(self, "d", init_value=d)
+            define_property(self, "theta", init_value=theta, can_set=False)
 
     def get_trans_mat(self) -> TransMatrix:
         ans = (
@@ -30,10 +58,3 @@ class LinkParam:
             @ get_rot4x4("x", self.alpha)
         )
         return zero_small_values(ans)
-
-    def set_val(self, val: float) -> float:
-        if self.is_rot_axis:
-            object.__setattr__(self, "theta", val)
-        else:
-            object.__setattr__(self, "d", val)
-        return val
