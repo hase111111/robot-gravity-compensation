e25814ba37be4eeb5372cf5b5bc27e7e07fab221
hase111111 <hasehasehase61@gmail.com>
2024-11-21 18:12:40 +0900

    link_paramに変数の最大値と最小値を追加

--- gravity_compensation/_robot/link_param.py
+++ gravity_compensation/_robot/link_param.py
@@ -1,16 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2023 Taisei Hasegawa
+# Released under the MIT license
+# https://opensource.org/licenses/mit-license.php
+
+
+import numpy as np
+
 from .._math.trans import get_rot4x4, get_trans4x4, zero_small_values
 from .._math.type import TransMatrix
 
 
 class LinkParam:
     def __init__(
-        self, a: float, alpha: float, d: float, theta: float, is_rot_axis: bool = True
+        self,
+        a: float,
+        alpha: float,
+        d: float,
+        theta: float,
+        *,
+        is_rot_axis: bool = True,
+        min_val: float = -np.pi,
+        max_val: float = np.pi,
     ):
         self._a = a
         self._alpha = alpha
         self.d = d
         self.theta = theta
         self._is_rot_axis = is_rot_axis
+        self._min_val = min_val
+        self._max_val = max_val
+
+    def get_trans_mat(self) -> TransMatrix:
+        ans = (
+            get_rot4x4("z", self.theta)
+            @ get_trans4x4(0.0, 0.0, self.d)
+            @ get_trans4x4(self.a, 0.0, 0.0)
+            @ get_rot4x4("x", self.alpha)
+        )
+
+        return zero_small_values(ans)
 
     @property
     def a(self):
@@ -36,12 +65,18 @@ class LinkParam:
     def is_rot_axis(self, _):
         pass
 
-    def get_trans_mat(self) -> TransMatrix:
-        ans = (
-            get_rot4x4("z", self.theta)
-            @ get_trans4x4(0.0, 0.0, self.d)
-            @ get_trans4x4(self.a, 0.0, 0.0)
-            @ get_rot4x4("x", self.alpha)
-        )
+    @property
+    def min_val(self):
+        return self._min_val
 
-        return zero_small_values(ans)
+    @min_val.setter
+    def min_val(self, _):
+        pass
+
+    @property
+    def max_val(self):
+        return self._max_val
+
+    @max_val.setter
+    def max_val(self, _):
+        pass
